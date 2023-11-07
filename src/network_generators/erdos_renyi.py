096287d9f7e6a524b7b56a5005caed16c29ea517
Blas Ko <kolic.blas@gmail.com>
2023-11-07 13:29:55 +0100

    erdos renyi generator

---
+++ src/network_generators/erdos_renyi.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+def generate_erdos_renyi_edgelist(n_nodes, n_edges=None, prob_edge=None):    
+    """Generate an erdos-renyi (pandas) edgelist using numpy (and not networkx).
+    """
+    
+    if n_edges is None:
+        n_edges = int(round(n_nodes * (n_nodes - 1) * prob_edge))
+
+    # Generate random pairs of connections
+    el = np.random.randint(0, n_nodes, size=(n_edges, 2))
+    
+    # Assign them to a dataframe
+    el = pd.DataFrame( el, columns=['source','target'] )
+
+    # Drop multi-edges
+    el = el.drop_duplicates(['source','target'])
+    
+    # Drop self-edges
+    el = el[ el['source'] != el['target'] ]
+
+    return el

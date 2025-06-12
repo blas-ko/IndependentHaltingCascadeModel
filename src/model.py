a821a02cb25b0e62e5372a3edbfb05127806d751
Blas Ko <kolic.blas@gmail.com>
2025-06-12 15:15:37 +0200

    Improved model documentation

--- src/model.py
+++ src/model.py
@@ -7,14 +7,15 @@ import pandas as pd
 # - Handle scipy.sparse matrices instead of the edgelist format. Slicing rows (sources) and columns (targets) should be more efficient for sparse matrices.
 class IndependentHaltingCascadeModel():
 
-    def __init__(self, edgelist, nodelist=None, activation_probs=None, application_probs=None, hiring_probs=None) -> None:
+    def __init__(self, edgelist, nodelist=None, recommendation_probs=None, application_probs=None, hiring_probs=None) -> None:
         """<X>_probs must be ordered in the same way than edgelist, or at least be a series with edge_ids as indexes.
 
         Args:
-            edgelist (_type_): _description_
-            activation_probs (_type_, optional): _description_. Defaults to None.
-            application_probs (_type_, optional): _description_. Defaults to None.
-            hiring_probs (_type_, optional): _description_. Defaults to None.
+            edgelist (pandas.dataframe): edgelist with columns ['source','target'] representing the network.
+            nodelist (pandas.dataframe, optional): nodelist where node_ids should be the index with columns ['application_probs','hiring_probs']. Defaults to None.
+            recommendation_probs ({float, itr}): Recommendation probabilities for each edge in the edgelist. Defaults to None.
+            application_probs ({float, itr}): _description_. Defaults to None.
+            hiring_probs ({float, itr}): _description_. Defaults to None.
         """                
         
         self.edgelist = edgelist.copy(deep=True)
@@ -25,29 +26,30 @@ class IndependentHaltingCascadeModel():
             nodelist = pd.DataFrame( index=nodelist )
         self.nodelist = nodelist
         
-        if activation_probs is not None:
-            self.edgelist['activation_probs'] = activation_probs
+        if recommendation_probs is not None:
+            self.edgelist['recommendation_probs'] = recommendation_probs
         if application_probs is not None:
             self.nodelist['application_probs'] = application_probs
         if hiring_probs is not None:
             self.nodelist['hiring_probs'] = hiring_probs
-        
+    
+    # TODO: Create a more verbose output (dictionary with keys 'diffusion_steps', 'applicants_steps', 'hires')?
     def simulate(
             self,
             initial_spreaders, 
-            max_diffusion_time=100, 
+            max_diffusion_time=100,
         ):
-        """Simulates the the independent cascade model using the `initial_spreaders` as seed nodes.
+        """Simulates the Independent Halting Cascades (IHC) model using `initial_spreaders` as seed nodes.
 
         Args:
-            edgelist (pandas.dataframe): network represented with an edgelist with columns ['source','target',activation_probability_col]
+            edgelist (pandas.dataframe): network represented with an edgelist with columns ['source','target',recommendation_probability_col]
             initial_spreaders (list): list of seed node ids. The ids should be in the 'source' column of `edgelist`.
-            max_diffusion_time (int, optional): Maximum cascade depth of the model after which the model stops. Defaults to 10.
-            activation_prob_col (str, optional): name of the column in edgelist containing the edge activation probabilities. Defaults to 'activation_probability'.
+            max_diffusion_time (int, optional): Maximum cascade depth of the model after which the model stops. Defaults to 100.
 
         Returns:
-            diffusion_steps (list): the i'th element of this list is a list with the nodes activated at the i'th step of the model.
-            activated_nodes (set): set of all node ids activated in the model simulation.
+            diffusion_steps (list): active recommenders at each step of the model, where element i gives the list of recommenders at step i
+            applicants_steps (list): applicants at each step of the model, where element i gives the list of applicants at step i
+            hires (list): list of successful hires
         """    
 
         # Create an internal (deep) copy of the edgelist (will be deleted at the end of the simulation)
@@ -91,10 +93,10 @@ class IndependentHaltingCascadeModel():
         """Takes one step of the independent cascade model based on the seed nodes `spreaders` and the `edgelist`.
 
         Args:
-            edgelist (pandas.dataframe): network represented with an edgelist with columns ['source','target',activation_probability_col]
+            edgelist (pandas.dataframe): network represented with an edgelist with columns ['source','target','recommendation_probability_col']
             initial_spreaders (list): list of seed node ids. The ids should be present in the 'source' column of `edgelist`.
             activated_nodes (set): set of nodes in the network who have been activated already, so we don't consider them.
-            activation_prob_col (str, optional): name of the column in edgelist containing the edge activation probabilities. Defaults to 'activation_probability'.
+            recommendation_prob_col (str, optional): name of the column in edgelist containing the edge activation probabilities. Defaults to 'recommendation_probability'.
             sources (list, optional): list of all possible spreaders in the network. nodes not in this list will be removed. If `sources==None`, no spreader is removed. Defaults to None.
 
         Returns:
@@ -121,8 +123,8 @@ class IndependentHaltingCascadeModel():
         #   - 2.1 Determine which users get activated according coin tosses
         spreaders_mask = self.tmp_edgelist.index.isin(spreaders)
         # Get pairs of possible (spreaders, targets) Note: targets could be repeated if they are neighbors of many spreaders.
-        spreaders_neighs = self.tmp_edgelist[spreaders_mask]        
-        activations_mask = self._get_activations( edgelist=spreaders_neighs )
+        spreaders_neighs = self.tmp_edgelist[spreaders_mask]
+        activations_mask = self._get_recommendations( edgelist=spreaders_neighs )
         #   - 2.2 Get the set of newly activated users
         activated_nodes = list( activations_mask[activations_mask].index.unique() )
         #   - 2.3 Get the nodes that will apply for a job (and therefore not continue the chain)
@@ -171,7 +173,7 @@ class IndependentHaltingCascadeModel():
             
             return hires
     
-    def _get_activations(self, edgelist, random_state=None):
+    def _get_recommendations(self, edgelist, random_state=None):
         """Compute which users get activated from a given edgelist with a column containing their activation probabilities.
 
         Args:
@@ -185,8 +187,8 @@ class IndependentHaltingCascadeModel():
         # Generate random activation thresholds
         activation_thresholds = np.random.rand( len(edgelist) )
         # Get the activation probability for each of the potential targets.
-        activation_probabilities = edgelist.set_index('target')['activation_probs']
+        recommendation_probabilities = edgelist.set_index('target')['recommendation_probs']
         # Determine which targets were activated and which ones were not
-        successful_activations = activation_probabilities > activation_thresholds
+        successful_activations = recommendation_probabilities > activation_thresholds
 
         return successful_activations
